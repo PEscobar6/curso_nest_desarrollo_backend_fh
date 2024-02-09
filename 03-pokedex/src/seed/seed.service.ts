8aa6a527ffdb4cf9c4d2efa62b92bf8bc983c281
Pablo Escobar <PESCOB3@bancodebogota.com.co>
2024-02-08 21:18:31 -0500

    insert many data into db

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel = Model<Pokemon>
+  ) {}
+
   public async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
-    const pokemons = data.results.map(({ name, url }) => {
-      const no = url.split('/').at(-2);
-      return { name, no };
-    });
+    await this.pokemonModel.deleteMany({});
 
-    return pokemons;
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=800');
+
+    const pokemons = data.results.map(({ name, url }) => ({
+      no: +url.split('/').at(-2),
+      name
+    }));
+
+    await this.pokemonModel.insertMany(pokemons);
+
+    return 'Seed executed successfully';
   }
 
 

a3460a9fa53a2f6a561c91c47b8dd96c5a068b45
Pablo Escobar <PESCOB3@bancodebogota.com.co>
2024-02-11 01:45:45 -0500

    End section 10

---
+++ 04-teslo-shop/src/products/entities/product.entity.ts
@@ -0,0 +1,57 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('products')
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', { unique: true })
+    title: string;
+
+    @Column('float', { default: 0 })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    @Column('text', {
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    checkSlugInsert() {
+        this.slug = this.title
+            .normalize('NFD') // split an accented letter in the base letter and the acent
+            .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
+            .toLowerCase()
+            .trim()
+            .replace(/[^a-z0-9 ]/g, '') // remove all chars not letters, numbers and spaces (to be replaced)
+            .replace(/\s+/g, '_')
+    }
+}
